21bed6f604b498ee4793b4410bc0c9f1a9b9ff63
viniciors12 <viniciorodriguez97@gmail.com>
2025-12-29 21:38:14 -0600

    all them should have the same code

--- CartonCapsReferrals/Services/ReferralStore.cs
+++ CartonCapsReferrals/Services/ReferralStore.cs
@@ -9,35 +9,39 @@ namespace CartonCapsReferrals.Api.Services
     {
         private readonly List<Referral> _referrals;
         private readonly object _lock = new();
+        private Dictionary<int, string> referralCodesByUser = new Dictionary<int, string>();
 
         public ReferralStore()
         {
             var faker = new Faker<Referral>()
-                .RuleFor(r => r.ReferralId, _ => Guid.NewGuid())
-                .RuleFor(r => r.ReferrerUserId, f => f.Random.Int(1, 10))
-                .RuleFor(r => r.Status, f => f.PickRandom(new[]
-                {
-                    ReferralStatus.Pending,
-                    ReferralStatus.Complete
-                }))
-                .RuleFor(r => r.RefereeUserId, (f, r) =>
-                    r.Status == ReferralStatus.Complete
-                        ? f.Random.Int(11, 20)
-                        : null)
-                .RuleFor(r => r.RefereeName, (f, r) =>
-                    r.Status == ReferralStatus.Complete
-                        ? f.Name.FullName()
-                        : string.Empty)
-                .RuleFor(r => r.ReferralCode, f => f.Random.AlphaNumeric(6).ToUpper())
-                .RuleFor(r => r.CreatedDt, f => f.Date.Recent(30))
-                .RuleFor(r => r.ModifiedDt, f => f.Date.Recent(10))
-                .RuleFor(r => r.ReferralLink, (f, r) => new ReferralLink
-                {
-                    ReferralLinkId = Guid.NewGuid(),
-                    Channel = f.PickRandom<Channel>(),
-                    DeepLinkUrl = $"app://cartoncaps/referralOnboarding?referral_code={r.ReferralCode}",
-                    ShortLinkUrl = $"https://cartoncaps.short.gy/{f.Random.AlphaNumeric(7)}",
-                });
+            .RuleFor(r => r.ReferralId, _ => Guid.NewGuid())
+            .RuleFor(r => r.ReferrerUserId, f => f.Random.Int(1, 10))
+            .RuleFor(r => r.Status, f => f.PickRandom(new[]
+            {
+                ReferralStatus.Pending,
+                ReferralStatus.Complete
+            }))
+            .RuleFor(r => r.RefereeUserId, (f, r) =>
+                r.Status == ReferralStatus.Complete
+                    ? f.Random.Int(11, 20)
+                    : null)
+            .RuleFor(r => r.RefereeName, (f, r) =>
+                r.Status == ReferralStatus.Complete
+                    ? f.Name.FullName()
+                    : string.Empty)
+            .RuleFor(r => r.ReferralCode, (f, r) =>
+                GetReferralCodeForUser(r.ReferrerUserId, f))
+            .RuleFor(r => r.CreatedDt, f => f.Date.Recent(30))
+            .RuleFor(r => r.ModifiedDt, f => f.Date.Recent(10))
+            .RuleFor(r => r.ReferralLink, (f, r) => new ReferralLink
+            {
+                ReferralLinkId = Guid.NewGuid(),
+                Channel = f.PickRandom<Channel>(),
+                DeepLinkUrl =
+                    $"app://cartoncaps/referralOnboarding?referral_code={r.ReferralCode}",
+                ShortLinkUrl =
+                    $"https://cartoncaps.short.gy/{f.Random.AlphaNumeric(7)}"
+            });
 
             _referrals = faker.Generate(50);
         }
@@ -63,6 +67,17 @@ namespace CartonCapsReferrals.Api.Services
             {
                 _referrals.Add(referral);
             }
+        }
+
+        private string GetReferralCodeForUser(int userId, Faker f)
+        {
+            if (!referralCodesByUser.TryGetValue(userId, out var code))
+            {
+                code = f.Random.AlphaNumeric(6).ToUpper();
+                referralCodesByUser[userId] = code;
+            }
+
+            return code;
         }
     }
 }
